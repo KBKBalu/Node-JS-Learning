6f8d85efb30b27e40c871ea49ab3fc0f7c067913
Balakrishna <iitbalu1993@gmail.com>
2017-07-04 15:19:31 +0530

    NodeJS is perfect for event-driven applications

---
+++ NodeJS/event.js
@@ -0,0 +1,14 @@
+var events = require('events');
+//create an EventEmitter object
+var eventEmitter = new events.EventEmitter();
+
+//Create an event handler
+var myEventHandler = function () {
+  console.log('I hear a scream!');
+}
+
+//Assign the event handler to an event
+eventEmitter.on('scream', myEventHandler);
+
+//Fire the 'scream' event
+eventEmitter.emit('scream');
