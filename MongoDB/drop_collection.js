a541c43ebd5c023896317658894114cfdc75a7b9
Balakrishna <iitbalu1993@gmail.com>
2017-07-05 11:14:39 +0530

    Drop Collection in MangoDB by using NodeJS
    We can delete a table, or collection as it is called in MongoDB, by using the "drop()" method
    We can also use the "dropCollection()" method to delete a table (collection)

---
+++ MongoDB/drop_collection.js
@@ -0,0 +1,11 @@
+var MongoClient = require('mongodb').MongoClient;
+var url = "mongodb://192.168.1.61:27017/mydb";
+
+MongoClient.connect(url, function(err, db) {
+  if (err) throw err;
+  db.dropCollection("customers", function(err, delOK) {
+    if (err) throw err;
+    if (delOK) console.log("Table deleted");
+    db.close();
+  });
+});
